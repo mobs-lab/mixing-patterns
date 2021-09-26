88a005de4ad416f02e05614595b88de4615d78ea
Dina Mistry <dina.c.mistry@gmail.com>
2021-09-25 19:35:52 -0700

    updated example - will add data last

--- analysis_results/scripts/make_matrices_diff_aggregations.py
+++ analysis_results/scripts/make_matrices_diff_aggregations.py
@@ -17,10 +17,11 @@ Note: In order to use the new matrices with a different age grouping, you'll nee
 an age distribution that is also mapped in the same grouping. Here, we only provide
 the age distribution for places mapped to the original 85 brackets and the ages
 aggregated to 18 brackets as an example. The methods below should show you how to
-write the matrices and age distribution to different age groupings.
+write the matrices and age distribution to different age groupings. Also provided
+are age mappings for 15 and 12 age brackets.
 
 """
-
+from glob import glob as ls
 import numpy as np
 import pandas as pd
 import copy
@@ -93,6 +94,8 @@ def read_contact_matrix(location, country, level, setting, num_agebrackets=85):
     Returns:
         A numpy matrix of contact.
     """
+    if country == location:
+        level = 'country'
     setting_type, setting_suffix = 'F', 'setting'
     if setting == 'overall':
         setting_type, setting_suffix = 'M', 'contact_matrix'
@@ -123,6 +126,8 @@ def get_ages(location, country, level, num_agebrackets=85):
     Returns:
         dict: A dictionary of the age count.
     """
+    if country == location:
+        level = 'country'
 
     if country == 'Europe':
         country = location
@@ -147,16 +152,16 @@ def get_available_age_brackets_and_mapping():
     brackets_dic = {}
     dict_of_age_by_brackets = {}
 
-    for num_agebrackets in [85, 20, 18, 15, 12]:
+    for num_agebrackets in [85, 18, 15, 12]:
         brackets = []
         if num_agebrackets == 85:
             for i in range(84):
                 brackets.append([i])
             brackets.append(np.arange(84, 101))
         # # num_agebracket = 20 only works if you have an age distribution and
-        # # matrices that go in more detail than age 84+, so if you wanted 
-        # # brackets of 85-89, 90-94, 95-100+, etc. it would be hard unless 
-        # # you have those matrices (which we don't because of the European 
+        # # matrices that go in more detail than age 84+, so if you wanted
+        # # brackets of 85-89, 90-94, 95-100+, etc. it would be hard unless
+        # # you have those matrices (which we don't because of the European
         # # matrices for Hungary and Portugal)
 
         # if num_agebrackets == 20:
@@ -191,11 +196,30 @@ def get_available_age_brackets_and_mapping():
 def get_age_brackets(available_age_brackets, age_by_brackets_mapping, num_agebrackets):
     """
     Return the age brackets and mapping of age to bracket for a specific mapping indexed by the number of brackets.
+
+    Args:
+        available_age_brackets (dict)  : a dictionary of available mappings
+        age_by_brackets_mapping (dict) : a dictionary of mappings for single years of age to the bracket or bin they map to depending on the number of age brackets defined
+        num_agebrackets (int)          : number of age brackets desired
+
+    Returns:
+        The age brackets and mapping of age to bracket for a specific mapping
+        indexed by the number of brackets.
     """
     return available_age_brackets[num_agebrackets], age_by_brackets_mapping[num_agebrackets]
 
 
 def get_aggregate_ages(ages, age_by_brackets_dic):
+    """
+    Return an aggregated age count distribution.
+
+    Args:
+        ages (dict)                : original age count distribution
+        age_by_brackets_dic (dict) : a dictionary mapping single years of age to the bracket or bin they map to
+
+    Returns:
+        An aggregated age count distribution from the mapping age_by_brackets_dic.
+    """
     num_agebrackets = len(set(age_by_brackets_dic.values()))
     aggregate_ages = dict.fromkeys(np.arange(num_agebrackets), 0)
     for a in ages:
@@ -205,8 +229,18 @@ def get_aggregate_ages(ages, age_by_brackets_dic):
 
 
 def get_aggregate_matrix(M, age_by_brackets_dic):
+    """
+    Return an aggregated age mixing contact matrix.
+
+    Args:
+        M (np.array): original age mixing contact matrix
+        age_by_brackets_dic (dict) : a dictionary mapping single years of age to the bracket or bin they map to
+
+
+    """
     N = len(M)
     num_agebrackets = len(set(age_by_brackets_dic.values()))
+    assert M >= num_agebrackets, 'The number of age brackets to aggregate to the matrix to is greater than the original matrix. We cannot aggregate the contact matrix with this mapping.'
     M_agg = np.zeros((num_agebrackets, num_agebrackets))
     for i in range(N):
         bi = age_by_brackets_dic[i]
@@ -236,6 +270,9 @@ def get_aggregate_symmetric_community_matrices(aggregate_ages):
     for a in aggregate_ages:
         M[a, :] *= float(aggregate_ages[a])
         M[:, a] *= float(aggregate_ages[a])
+    # each individual can interact with others in their own age group, but we shouldn't count self interactions
+    for a in aggregate_ages:
+        M[a, a] -= aggregate_ages[a]
     return M
 
 
@@ -278,7 +315,7 @@ def get_new_aggregate_matrices(location, num_agebrackets, available_age_brackets
     return new_matrices
 
 
-def write_new_aggregated_matrices(location, new_matrices, datadir):
+def write_new_aggregated_matrices(location, new_matrices, datadir, overwrite=False):
     num_agebrackets = len(new_matrices[list(new_matrices.keys())[0]])
     country = get_country_name(locations_df, location)
 
@@ -290,7 +327,7 @@ def write_new_aggregated_matrices(location, new_matrices, datadir):
     else:
         level = 'subnational'
 
-    for layer in ['household', 'school', 'work', 'community']:
+    for layer in new_matrices.keys():
         setting = layer
         setting_type, setting_suffix = 'F', 'setting'
         if setting == 'overall':
@@ -301,8 +338,16 @@ def write_new_aggregated_matrices(location, new_matrices, datadir):
         else:
             file_name = f"{country}_{level}_{location}_{setting_type}_{setting}_{setting_suffix}_{num_agebrackets:.0f}.csv"
         file_path = os.path.join(datadir, 'contact_matrices', file_name)
-        print(location, layer, new_matrices[setting].sum() / num_agebrackets)
-        np.savetxt(file_path, new_matrices[setting], fmt='%.16f', delimiter=',')
+        e = os.path.exists(file_path)
+        if e:
+            print(f'{file_path} already exists.')
+            if overwrite:
+                print('Overwriting the file.')
+                np.savetxt(file_path, new_matrices[setting], fmt='%.16f', delimiter=',')
+            else:
+                print('Not overwriting the existing file.')
+        else:
+            np.savetxt(file_path, new_matrices[setting], fmt='%.16f', delimiter=',')
     return
 
 
@@ -323,21 +368,78 @@ def write_new_aggregated_ages(location, aggregate_ages, datadir):
     else:
         file_name = f"{country}_{level}_{location}_age_distribution_{num_agebrackets:.0f}.csv"
     file_path = os.path.join(datadir, 'age_distributions', file_name)
-    # print(file_path)
     f = open(file_path, 'w+')
     for a in range(len(aggregate_ages)):
         f.write(f"{a:.16f},{aggregate_ages[a]:.16f}\n")
     return
 
 
+def check_matrix_files_delimiter(num_agebrackets=85):
+    matrix_files = ls(os.path.join(datadir, 'contact_matrices', f'*{num_agebrackets:.0f}*'))
+    matrix_files = sorted(matrix_files)
+    s = False
+    for file_path in matrix_files:
+        try:
+            M = np.loadtxt(file_path, delimiter=',')
+        except:
+            print(f'{file_path} has a different delimiter than the expected ",".')
+            s = True
+    if s:
+        print('Found at least one contact matrix using a different delimiter than the expected ",".')
+
+
+def check_age_distribution_count(locations, num_agebrackets=18):
+    for location in locations:
+        country = get_country_name(locations_df, location)
+        level = 'subnational'
+        ages_85 = get_ages(location, country, level, num_agebrackets=85)
+        ages = get_ages(location, country, level, num_agebrackets=num_agebrackets)
+        # print(location, country)
+        assert sum(ages.values()) == sum(ages_85.values()), f'Sum of aggregated ages ({num_agebrackets} age brackets) does not match original age count (85 age brackets).'
+
+
+def combine_matrices(location, num_agebrackets=85, weights=None, verbose=False):
+    country = get_country_name(locations_df, location)
+    level = 'subnational'
+
+    matrices = dict()
+    matrices['overall'] = np.zeros((num_agebrackets, num_agebrackets))
+
+    # use default weights if none provided - see paper for details on how these were derived
+    if weights is None:
+        weights = dict()
+        weights['household'] = 4.1103
+        weights['school'] = 11.4069
+        weights['work'] = 8.0746
+        weights['community'] = 2.7868
+
+    elif not isinstance(weights, dict):
+        raise ValueError('weights must be a dictionary.')
+
+    for l in ['household', 'school', 'work', 'community']:
+        matrices[l] = read_contact_matrix(location, country, level, l, num_agebrackets)
+        matrices['overall'] += matrices[l] * weights[l]
+
+    if verbose:
+        print(location, country, matrices['overall'].sum() / num_agebrackets)
+
+    for l in ['household', 'school', 'work', 'community']:
+        matrices.pop(l)
+
+    return matrices
+
+
 if __name__ == '__main__':
 
+    # find out what age brackets and mappings are available -- if you want to use different ones than available, look at the details of this method to add them
     available_age_brackets, age_by_brackets_mapping = get_available_age_brackets_and_mapping()
 
+    # let's create contact matrices aggregated to 18 age brackets: 5 year bins, where the last bin is 84 years old and up
     num_agebrackets = 18
     age_brackets = available_age_brackets[num_agebrackets]
     age_by_brackets_dic = age_by_brackets_mapping[num_agebrackets]
 
+    # get all of the locations possible from the countries/regions we provide data for
     countries = ['Australia', 'Canada', 'China', 'Europe', 'India', 'Israel', 'Japan', 'Russia', 'South_Africa', 'United_States']
     locations = []
 
@@ -346,24 +448,38 @@ if __name__ == '__main__':
         if country not in locations and country not in ['Russia', 'Europe']:
             locations.append(country)
 
+        # a few territories in India we did not end up modeling
         if country == 'India':
             locations.remove('Dadra_and_Nagar_Haveli')
             locations.remove('Chandigarh')
             locations.remove('Lakshadweep')
 
-    write_flag = True
-    # write_flag = False
+    # first a check: all the original matrix files for 85 age brackets have ',' as the delimiter
+    # check_matrix_files_delimiter(85)
 
-    for li, location in enumerate(locations):
-        print(location, country)
-        country = get_country_name(locations_df, location)
-        if location == 'Israel' or location == country:
-            ages = get_ages(location, country, 'country')
-        else:
-            ages = get_ages(location, country, 'subnational')
-        aggregate_ages = get_aggregate_ages(ages, age_by_brackets_dic)
+    # next: write the setting matrices aggregated to num_agebrackets
+    write_flag = False  # set to True to save matrices to disk
+    if write_flag:
+        for location in locations:
+            country = get_country_name(locations_df, location)
+            if location == 'Israel' or location == country:
+                ages = get_ages(location, country, 'country')
+            else:
+                ages = get_ages(location, country, 'subnational')
+            aggregate_ages = get_aggregate_ages(ages, age_by_brackets_dic)
 
-        new_matrices = get_new_aggregate_matrices(location, num_agebrackets, available_age_brackets, age_by_brackets_mapping)
-        if write_flag:
+            new_matrices = get_new_aggregate_matrices(location, num_agebrackets, available_age_brackets, age_by_brackets_mapping)
             write_new_aggregated_matrices(location, new_matrices, datadir)
             write_new_aggregated_ages(location, aggregate_ages, datadir)
+
+    # check those matrices use the correct delimiter of ','
+    check_matrix_files_delimiter(num_agebrackets=num_agebrackets)  # this will only work if you've already run the code with write_flag = True before or at the same time
+    # check the sum of the aggregated age distribution count for num_agebrackets matches the original count
+    check_age_distribution_count(locations, num_agebrackets=num_agebrackets)
+
+    # write the overall matrix for the new aggregation
+    write_flag = False  # set to True to save matrices to disk
+    if write_flag:
+        for location in locations:
+            matrix = combine_matrices(location, num_agebrackets=num_agebrackets)  # just returns a new overall matrix (weighted linear combination) for num_agebrackets
+            write_new_aggregated_matrices(location, matrix, datadir)
